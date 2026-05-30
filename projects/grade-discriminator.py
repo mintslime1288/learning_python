0bcc6c796bde52063b8ddfde05f9d595e504794b
mintslime1288 <mintslime1288@gmail.com>
2026-05-31 03:43:55 +0900

    if문만을 사용하여 성적 판별기를 만들었습니다. created a grade reader using only if statement

---
+++ projects/grade-discriminator.py
@@ -0,0 +1,11 @@
+studentgrades=float(input("Enter the student's grade: "));
+if studentgrades>=90:
+    print("The student's grade is A.");
+elif studentgrades>=80:
+    print("The student's grade is B.");
+elif studentgrades>=70:
+    print("The student's grade is C.");
+elif studentgrades>=60:
+    print("The student's grade is D.");
+else:
+    print("The student's grade is F.");
